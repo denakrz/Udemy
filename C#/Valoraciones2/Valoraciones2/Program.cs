38be5c93e483d6f3fb033612ad9073976abf249b
Denise Krzyny <denisek@lagash.com>
2019-01-04 17:15:10 -0300

    agrego system.speech

--- C#/Valoraciones2/Valoraciones2/Program.cs
+++ C#/Valoraciones2/Valoraciones2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech;
+using System.Speech.Synthesis;
 
 namespace Valoraciones2
 {
@@ -12,6 +14,7 @@ namespace Valoraciones2
         {
             // crear objeto libro
             LibroValoraciones libro = new LibroValoraciones();
+            SpeechSynthesizer hablar = new SpeechSynthesizer();
 
             // asigno valoraciones
             libro.AgregarValoracion(1.2f); // le agrego una f porque es un float.
@@ -21,7 +24,7 @@ namespace Valoraciones2
 
 
             // publicar valoraciones
-
+            hablar.Speak("Este es el resultado de las valoraciones");
             CalcularValoraciones publicar = libro.PublicarValoraciones();
             Console.WriteLine("El promedio es: " + publicar.promedioValoraciones); // 3,65
             Console.WriteLine("El valor mínimo es: " + publicar.valoracionMin); // 1.2
