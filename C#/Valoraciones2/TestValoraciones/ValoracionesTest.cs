c87b6eb8040c2761bc5de3b696a7b6a78aafdc14
Denise Krzyny <denisek@lagash.com>
2019-01-04 17:55:14 -0300

    tests

--- C#/Valoraciones2/TestValoraciones/ValoracionesTest.cs
+++ C#/Valoraciones2/TestValoraciones/ValoracionesTest.cs
@@ -15,11 +15,43 @@ namespace TestValoraciones
         public void CalcularValoracionMax()
         {
             LibroValoraciones libro = new LibroValoraciones();
+            libro.AgregarValoracion(1.2f); 
             libro.AgregarValoracion(5);
+            libro.AgregarValoracion(5);
+            libro.AgregarValoracion(3.4f);
+
             CalcularValoraciones resultado = libro.PublicarValoraciones();
 
             Assert.AreEqual(5, resultado.valoracionMax);
         }
-        
+
+        [TestMethod]
+        public void CalcularValoracionMin()
+        {
+            LibroValoraciones libro = new LibroValoraciones();
+            libro.AgregarValoracion(1.2f);
+            libro.AgregarValoracion(5);
+            libro.AgregarValoracion(5);
+            libro.AgregarValoracion(3.4f);
+
+            CalcularValoraciones resultado = libro.PublicarValoraciones();
+
+            Assert.AreEqual(1.2f, resultado.valoracionMin);
+        }
+
+        [TestMethod]
+        public void CalcularValoracionPromedio()
+        {
+            LibroValoraciones libro = new LibroValoraciones();
+            libro.AgregarValoracion(1.2f);
+            libro.AgregarValoracion(5);
+            libro.AgregarValoracion(5);
+            libro.AgregarValoracion(3.4f);
+
+            CalcularValoraciones resultado = libro.PublicarValoraciones();
+
+            Assert.AreEqual(3.65f, resultado.promedioValoraciones,0.01);
+        }
+
     }
 }
