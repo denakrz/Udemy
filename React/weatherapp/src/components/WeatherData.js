2d81b483ebd84805193cba02ae9499e3b363a7db
Denise Krzyny <denisek@lagash.com>
2019-01-06 17:41:49 -0300

    Curso React

---
+++ React/weatherapp/src/components/WeatherData.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeatherTeperature from './WeatherTemperature';
+import WeatherExtraInfo from './WeatherExtraInfo';
+
+const WeatherData = () => (
+   <div>
+       <WeatherTeperature />
+        <WeatherExtraInfo />
+    </div>
+);
+
+export default WeatherData;
